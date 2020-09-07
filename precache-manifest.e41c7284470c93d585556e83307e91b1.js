86d8c914ff370ae39c61de4cc278a7a97ce0d2de
Sachin Singh Negi <negisachin688@gmail.com>
2020-09-07 15:42:31 +0530

    Updates

---
+++ precache-manifest.e41c7284470c93d585556e83307e91b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01cbce2b6deed16fcb9174fca32c3b65",
+    "url": "/Rest-Countries/index.html"
+  },
+  {
+    "revision": "4a493c611e789a5128fb",
+    "url": "/Rest-Countries/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "343085950e0cf4983304",
+    "url": "/Rest-Countries/static/css/main.3b7a448a.chunk.css"
+  },
+  {
+    "revision": "4a493c611e789a5128fb",
+    "url": "/Rest-Countries/static/js/2.34e75568.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rest-Countries/static/js/2.34e75568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "343085950e0cf4983304",
+    "url": "/Rest-Countries/static/js/main.6df3dff0.chunk.js"
+  },
+  {
+    "revision": "fd48da2e60d222769d32",
+    "url": "/Rest-Countries/static/js/runtime-main.d46cbf9a.js"
+  }
+]);
